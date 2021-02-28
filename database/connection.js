395eac12d57ddf06720082c4bc03402230728783
deadair1998 <trungnam2101998@gmail.com>
2021-02-28 22:56:03 +0700

    Setup database connection

--- database/connection.js
+++ database/connection.js
@@ -19,6 +19,7 @@ module.exports = {
     database: "dtg1p7o6631g7",
     host: "ec2-54-87-34-201.compute-1.amazonaws.com",
     dialect: "postgres",
-    ssl: true
+    ssl: true,
+    url: 'postgres://khrhmbscaqiwyi:d46a2d5fcacc09168d07fd08d0a967c268aa4a754b477411cfc795311394c70b@ec2-54-87-34-201.compute-1.amazonaws.com:5432/dtg1p7o6631g7?ssl=true'
   },
 };
