51e30528bc7af84a2c06d711cda06779bfc445a5
ngomy2001 <ngomy2001@gmail.com>
2021-03-02 13:26:50 +0700

    Setup database connection

--- database/connection.js
+++ database/connection.js
@@ -1,25 +1,25 @@
 module.exports = {
-  development: {
-    username: "postgres",
-    password: "deadcenter123",
-    database: "ASM2_PHAMTRUNGNAM_ATN",
-    host: "localhost",
-    dialect: "postgres",
-  },
-  test: {
-    username: "root",
-    password: "11223311",
-    database: "mati_test",
-    host: "172.15.0.2",
-    dialect: "postgres",
-  },
-  production: {
-    username: "khrhmbscaqiwyi",
-    password: "d46a2d5fcacc09168d07fd08d0a967c268aa4a754b477411cfc795311394c70b",
-    database: "dtg1p7o6631g7",
-    host: "ec2-54-87-34-201.compute-1.amazonaws.com",
-    dialect: "postgres",
-    ssl: true,
-    url: 'postgres://khrhmbscaqiwyi:d46a2d5fcacc09168d07fd08d0a967c268aa4a754b477411cfc795311394c70b@ec2-54-87-34-201.compute-1.amazonaws.com:5432/dtg1p7o6631g7'
-  },
+    development: {
+        username: "postgres",
+        password: "deadcenter123",
+        database: "ASM2_PHAMTRUNGNAM_ATN",
+        host: "localhost",
+        dialect: "postgres",
+    },
+    test: {
+        username: "root",
+        password: "11223311",
+        database: "mati_test",
+        host: "172.15.0.2",
+        dialect: "postgres",
+    },
+    production: {
+        username: "jcectkoghywbdc",
+        password: "08e926a8987d0ad52ce9f88eb56644ab7c2938e534a0f3a513eb03b267ad5aad",
+        database: "d74jr2g15tqguc",
+        host: "ec2-18-211-97-89.compute-1.amazonaws.com",
+        dialect: "postgres",
+        ssl: true,
+        url: 'postgres://jcectkoghywbdc:08e926a8987d0ad52ce9f88eb56644ab7c2938e534a0f3a513eb03b267ad5aad@ec2-18-211-97-89.compute-1.amazonaws.com:5432/d74jr2g15tqguc'
+    },
 };
